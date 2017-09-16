4bd7000d5caad9a76283dc91b8eea617e3409b76
Seth <swvn10@gmail.com>
2017-09-16 10:26:58 -0400

    [etc] I'm an idiot, revert git change
    also clean up the info tab on the dashboard

--- rowboat/views/dashboard.py
+++ rowboat/views/dashboard.py
@@ -111,8 +111,7 @@ def deploy():
     if not g.user.admin:
         return '', 401
 
-#    subprocess.Popen(['git', 'pull', 'origin', 'master']).wait()
-    subprocess.Popen(['git', 'pull']).wait()
+    subprocess.Popen(['git', 'pull', 'origin', 'master']).wait()
     rdb.publish('actions', json.dumps({
         'type': 'RESTART',
     }))
